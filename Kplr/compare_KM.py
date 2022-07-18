eef5a8cdd306bb07517021f5410203a4c1390512
ani <best19980102@gmail.com>
2022-07-18 10:49:22 +0800

    To compare magnitude.

--- Kplr/compare_KM.py
+++ Kplr/compare_KM.py
@@ -1,8 +1,13 @@
+'''
+This file is to record and compare magnitude of datas we had chosen from Kepler Q1 dataset.
+'''
+
 import numpy as np
 import matplotlib.pylab as plt
 import os
 
 def ReadTxtKm():
+    '''collect all datas' magnitude, and save as txt file.'''
     path = os.getcwd()
     dirs = os.listdir(path)
     print(dirs)
@@ -13,7 +18,7 @@ def ReadTxtKm():
             Km.append(KM)
     np.savetxt('KM.txt', Km,fmt='%10.3f')
 
-#ReadTxtKm()
+ReadTxtKm()
 
 data=np.loadtxt('KM.txt',unpack=True)
 part_data14=[data[i] for i in np.where(data<14)[0]]
@@ -23,8 +28,6 @@ plt.hist(data,bins='auto',alpha=0.3,cumulative=True,label='Cumulation: KM>14')
 plt.hist(part_data14,bins='auto',alpha=0.3,cumulative=True,label='Cumulation: 13<KM<14')
 plt.hist(part_data13,bins='auto',alpha=0.3,cumulative=True,label='Cumulation: KM<13')
 
-#n,bins,patches=plt.hist(data,bins='auto',label='Number of each interval')
-#plt.text(16,10000,'(a)',size=12)
 plt.annotate(str(len(data)-len(part_data14)),
              xy=(15.5,8000),xytext=(13.5,8000),
              arrowprops=dict(arrowstyle='->'),
@@ -37,24 +40,9 @@ plt.annotate(str(len(part_data13)),
              xy=(12.5,650),xytext=(10.5,650),
              arrowprops=dict(arrowstyle='->'),
              bbox=dict(boxstyle='round,pad=0.5',fc='green',alpha=0.2))
-#plt.title('(a)')
+
 plt.legend()
 plt.xlabel('Magnitude')
 plt.ylabel('Number')
 plt.savefig('KM_cumulation_hist')
 plt.show()
-
-'''
-
-plt.hist(part_data14,bins='auto',label='Number of each interval',histtype='bar')
-plt.text(6,100,'(a)',size=14)
-plt.text(12,2500,'Total: '+str(len(part_data14)),size=12,color='black')
-plt.text(11,700,'Total: '+str(len(part_data13)),size=12,color='black')
-
-
-#plt.ylim(0,int(max(n)))
-plt.legend()
-plt.xlabel('Magnitude')
-plt.ylabel('Number')
-plt.savefig('KM_each_hist')
-plt.show()'''
